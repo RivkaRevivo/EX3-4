7b4a059b3e29a503074bffac2165e6bdd871df59
sela27 <sela@goldenberg.xyz>
2018-12-04 18:12:22 +0200

    testing commit and push by sela

--- src/File_format/Main.java
+++ src/File_format/Main.java
@@ -2,6 +2,8 @@ package File_format;
 
 public class Main {
 
+	//a commit to test commit and push by sela
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Csv2kml no=new Csv2kml("C:\\file\\WigleWifi_20171201110209.csv");
