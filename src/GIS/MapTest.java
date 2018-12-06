aaa6149cc9a524ffd65fa4aed1f3c0c5d96e62d8
shai <kaikhhhh@gmail.com>
2018-12-07 00:47:57 +0200

    try

--- src/GIS/MapTest.java
+++ src/GIS/MapTest.java
@@ -1,25 +1,45 @@
 package GIS;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import Geom.Point3D;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class MapTest {
 
-class MapTest {
+	@Test
+	public void testMap() {
+		fail("Not yet implemented");
+	}
 
-    @org.junit.jupiter.api.Test
-    void pixelToCoordinate()
-    {
-    }
+	@Test
+	public void testPixelToCoordinate() {
+		fail("Not yet implemented");
+	}
 
-    @org.junit.jupiter.api.Test
-    void coordinateToPixel()
-    {
-        Point3D max= new Point3D(220190,667650);
-        Point3D min = new Point3D(219287, 668008);
-        String img = "Ariel1.png";
-        Map m = new Map(max,min,img);
-        Pixel p = m.CoordinateToPixel(max);
-        System.out.println("x: " + p.getX() + " y:" + p.getY());
-        int y;
-    }
+	@Test
+	public void testCoordinateToPixel() {
+	  
+	        Point3D max= new Point3D(220190,667650);
+	        Point3D min = new Point3D(219287, 668008);
+	        String img = "Ariel1.png";
+	        Map m = new Map(max,min,img);
+	        Pixel p = m.CoordinateToPixel(max);
+	        System.out.println("x: " + p.getX() + " y:" + p.getY());
+	        
+	    }
+	
+	
+
+	@Test
+	public void testHaversine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAzimuth() {
+		fail("Not yet implemented");
+	}
+
 }
