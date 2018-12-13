089dc00f336007f6feea01edf922821d875bb2b8
shai <kaikhhhh@gmail.com>
2018-12-13 17:43:44 +0200

    Try to Commit in order to pull

--- src/Algorithms/Test.java
+++ src/Algorithms/Test.java
@@ -9,9 +9,9 @@ public class Test {
 		 * First you need to define where you want to create the File and its name. 
 		 * For Example: ("/home/eli/eclipse-workspace/OOP_EX2-EX4-master/data/MultiCSV.kml")
 		 */
-		MultiCSV Test = new MultiCSV();
+		/*MultiCSV Test = new MultiCSV();
 		File MultiCSV = new File("C:\\file\\MultiCSV.kml");
-		Test.ListOfCSV(MultiCSV);
+		Test.ListOfCSV(MultiCSV);*/
 		
 		
 	}
