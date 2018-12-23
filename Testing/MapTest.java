6d64dc4038af850740fa01fafde35a84555575f6
sela27 <sela@goldenberg.xyz>
2018-12-23 22:48:03 +0200

    Added Test And Some javadoc

--- Testing/MapTest.java
+++ Testing/MapTest.java
@@ -1,69 +1,126 @@
-import Factory.MapFactory;
 import GIS.Map;
 import GIS.Pixel;
 import Geom.Point3D;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.fail;
+
 public class MapTest {
 
-    @Test
-    public void testMap()
-    {
-        //fail("Not yet implemented");
-    }
+
 
     @Test
-    public void testPixelToCoordinate()
-    {
-        /*
-        Point3D max= new Point3D(220190,667650);
-        Point3D min = new Point3D(219287, 668008);
-        String img = "Ariel1.png";
-        Map m = new Map(max,min,img);
-        Pixel p = new Pixel(612,264);
-        Point3D po = m.PixelToCoordinate(p);
-        System.out.println("x: " + po.x() + " y: " + po.y());
-        */
-    }
+    public void testPixelToCoordinatePixel() {
+        Point3D West_North=new Point3D(35.202574,32.106046);
+        Point3D East_South=new Point3D(35.212405,32.101658);
+        Map m=new Map(East_South,West_North,"Ariel1.png");
+        Pixel P0=new Pixel(0,0);
+        Point3D NewP0=m.PixelToCoordinate(P0);
 
-    @Test
-    public void testCoordinateToPixel()
-    {
+        if(NewP0.x()!=35.202574 || NewP0.y()!=32.106046) {
+            fail("Not implemented good");
+        }
+        //cheak in half of resolution-need to get near 716*321
 
-        Map m = MapFactory.ArielMap();
-        Point3D po = new Point3D(32.11317315828321 , 35.21093728088261);
-        Pixel p = m.CoordinateToPixel(po);
-        System.out.println("x: " + p.getX() + " y:" + p.getY());
+        Pixel P1=new Pixel(716,321);
+        Point3D NewP1=m.PixelToCoordinate(P1);
+        //the range that need to be in coordinate
+        if(NewP1.x()<35.207 ||NewP1.x()>35.208 || NewP1.y()<32.103 ||NewP1.y()>32.104 ) {
+            fail("Not implemented good");
+        }
 
 
     }
 
-
-
     @Test
-    public void testHaversine()
-    {
-        /*Point3D p1 = new Point3D(32.103315, 35.209039 , 670);
-        Point3D p2 = new Point3D(32.106352, 35.205225, 650);
-        Point3D max= new Point3D(220190,667650);
-        Point3D min = new Point3D(219287, 668008);
-        String img = "Ariel1.png";
-        Map m = new Map(max,min,img);
-        Pixel pi1 = m.CoordinateToPixel(p1);
-        Pixel pi2  = m.CoordinateToPixel(p2);
-        System.out.println(m.haversine(pi1,pi2));*/
+    public void testPixelToCoordinatePixelIntInt() {
+        Point3D West_North=new Point3D(35.202574,32.106046);
+        Point3D East_South=new Point3D(35.212405,32.101658);
+        Map m=new Map(East_South,West_North,"Ariel1.png");
+        Pixel P0=new Pixel(0,0);
+        Point3D NewP0=m.PixelToCoordinate(P0,1433,642);
+
+        if(NewP0.x()!=35.202574 || NewP0.y()!=32.106046) {
+            fail("Not implemented good");
+        }
+        //cheak in half of resolution-need to get near 716*321
+
+        Pixel P1=new Pixel(716,321);
+        Point3D NewP1=m.PixelToCoordinate(P1,1433,642);
+        //the range that need to be in coordinate
+        if(NewP1.x()<35.207 ||NewP1.x()>35.208 || NewP1.y()<32.103 ||NewP1.y()>32.104 ) {
+            fail("Not implemented good");
+        }
+
     }
 
     @Test
-    public void testAzimuth()
-    {
-        //fail("Not yet implemented");
+    public void testCoordinateToPixelPoint3D() {
+        Point3D West_North=new Point3D(35.202574,32.106046);
+        Point3D East_South=new Point3D(35.212405,32.101658);
+        Map m=new Map(East_South,West_North,"Ariel1.png");
+        Pixel NewP0=m.CoordinateToPixel(East_South);
+
+        if(NewP0.getX()!=1433 || NewP0.getY()!=642) {
+            fail("Not implemented good");
+        }
+        //cheak in half of resolution-need to get near 716*321
+        Point3D Cheakp=new Point3D(35.207374,32.103971);
+        Pixel NewP1=m.CoordinateToPixel(Cheakp);
+        //the range that need to be in pixel
+        if(NewP1.getX()>750 ||NewP1.getX()<650 || NewP1.getY()>350|| NewP1.getY()<250) {
+            fail("Not implemented good");
+        }
+
+
     }
 
     @Test
-    public void testSizeOfPixleInMeter()
-    {
-        Map m = MapFactory.ArielMap();
-        System.out.println("Size Of Pixle In Meter:  "+m.SizeOfPixleInMeter());
+    public void testCoordinateToPixelPoint3DIntInt() {
+        Point3D West_North=new Point3D(35.202574,32.106046);
+        Point3D East_South=new Point3D(35.212405,32.101658);
+        Map m=new Map(East_South,West_North,"Ariel1.png");
+        Pixel NewP0=m.CoordinateToPixel(East_South,1433,642);
+
+        if(NewP0.getX()!=1433 || NewP0.getY()!=642) {
+            fail("Not implemented good");
+        }
+        //cheak in half of resolution-need to get near 716*321
+        Point3D Cheakp=new Point3D(35.207374,32.103971);
+        Pixel NewP1=m.CoordinateToPixel(Cheakp,1433,642);
+        //the range that need to be in pixel
+        if(NewP1.getX()>750 ||NewP1.getX()<650 || NewP1.getY()>350|| NewP1.getY()<250) {
+            fail("Not implemented good");
+        }
+
+
     }
+
+
+
+    @Test
+    public void testAzimuth() {
+        Pixel P0=new Pixel(716,321);
+        Pixel P1=new Pixel(477,214);
+        Point3D West_North=new Point3D(35.202574,32.106046);
+        Point3D East_South=new Point3D(35.212405,32.101658);
+        Map m=new Map(East_South,West_North,"Ariel1.png");
+        //this is the coordinate that converted from pixels
+		/*35.20748606978367
+		32.103852
+		35.20584642637822
+		32.10458333333333*/
+
+        double A=m.Azimuth(P0, P1);
+        //range that A need to be in angle
+        if(A>2.81 || A<2.78) {
+            fail("Not implemented good");
+        }
+
+    }
+
+
+
+
+
 }
--- src/GIS/Map.java
+++ src/GIS/Map.java
@@ -20,117 +20,120 @@ public class Map
 
 
 
-	Image map;            //image
-	Point3D max,min;      //coordinates in degrees
-	private Pixel Pmax,Pmin; //pixels
+    Image map;            //image
+    Point3D max,min;      //coordinates in degrees
+    private Pixel Pmax,Pmin; //pixels
 
-	/**
-	 * construct of path of the map and points of max and min of degrees.
-	 * in the constructs we get the max pixel and min pixel(0,0) from the image
-	 * @param max of coordinate in degree in axis of x and y(in right dowen of the map)
-	 * @param min of coordinate in degree in axis of x and y(in left up of the map)
-	 * @param img is the path string that we get in the function
-	 */
-	public Map(Point3D max , Point3D min, String img)
-	{
-		this.max = new Point3D(max);
-		this.min = new Point3D(min);
-		this.Pmax = new Pixel();
-		this.Pmin = new Pixel();
+    /**
+     * construct of path of the map and points of max and min of degrees.
+     * in the constructs we get the max pixel and min pixel(0,0) from the image
+     * @param max of coordinate in degree in axis of x and y(in right dowen of the map)
+     * @param min of coordinate in degree in axis of x and y(in left up of the map)
+     * @param img is the path string that we get in the function
+     */
+    public Map(Point3D max , Point3D min, String img)
+    {
+        this.max = new Point3D(max);
+        this.min = new Point3D(min);
+        this.Pmax = new Pixel();
+        this.Pmin = new Pixel();
 
-		try
-		{
-			map = ImageIO.read(new File(img));
-			this.Pmax.setX(((BufferedImage) map).getWidth());       //take the point max from the map(in pixel-width)
-			this.Pmax.setY(((BufferedImage) map).getHeight());      //take the point max from the map(in pixel-height)
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+        try
+        {
+            map = ImageIO.read(new File(img));
+            this.Pmax.setX(((BufferedImage) map).getWidth());       //take the point max from the map(in pixel-width)
+            this.Pmax.setY(((BufferedImage) map).getHeight());      //take the point max from the map(in pixel-height)
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
-		this.Pmin.setX(0);                //set point min (0,0)
-		this.Pmin.setY(0);
+        this.Pmin.setX(0);                //set point min (0,0)
+        this.Pmin.setY(0);
 
 
 
-	}
+    }
 
-	/**
-	 *function that convert pixel(point of x and y) to coordinate
-	 * @param p of pixel
-	 * @return ThisPoint-the new point in degree
-	 */
+    /**
+     *function that convert pixel(point of x and y) to coordinate
+     * @param p of pixel
+     * @return ThisPoint-the new point in degree
+     */
 
-	public Point3D PixelToCoordinate(Pixel p)
-	{
-		double MinMaxdiff_X = max.x() - min.x();
-		double MinMaxdiff_Y = max.y() - min.y();
+    public Point3D PixelToCoordinate(Pixel p)
+    {
+        double MinMaxdiff_X = max.x() - min.x();
+        double MinMaxdiff_Y = max.y() - min.y();
 
-		double diffCoord_X=(MinMaxdiff_X* p.getX())/this.Pmax.getX();
-		double diffCoord_Y=(MinMaxdiff_Y*p.getY())/this.Pmax.getY();
+        double diffCoord_X=(MinMaxdiff_X* p.getX())/this.Pmax.getX();
+        double diffCoord_Y=(MinMaxdiff_Y*p.getY())/this.Pmax.getY();
 
-		double FullCoord_X=diffCoord_X+this.min.x();
-		double FullCoord_Y=diffCoord_Y+this.min.y();
+        double FullCoord_X=diffCoord_X+this.min.x();
+        double FullCoord_Y=diffCoord_Y+this.min.y();
 
-		Point3D ThisPoint=new Point3D(FullCoord_X,FullCoord_Y);
-		return ThisPoint;
-	}
+        Point3D ThisPoint=new Point3D(FullCoord_X,FullCoord_Y);
+        return ThisPoint;
+    }
 
-	/**
-	 * function that convert pixel(point of x and y) to coordinate
-	 * @param p of pixel
-	 * @param width of the map that we get from the width of the map
-	 * @param height of the map that we get from the height of the map
-	 * @return point in degree(x,y)
-	 */
-	public Point3D PixelToCoordinate(Pixel p, int width, int height)
-	{
-		double MinMaxdiff_X = max.x() - min.x();
-		double MinMaxdiff_Y = max.y() - min.y();
+    /**
+     * function that convert pixel(point of x and y) to coordinate
+     * @param p of pixel
+     * @param width of the map that we get from the width of the map
+     * @param height of the map that we get from the height of the map
+     * @return point in degree(x,y)
+     */
+    public Point3D PixelToCoordinate(Pixel p, int width, int height)
+    {
+        double MinMaxdiff_X = max.x() - min.x();
+        double MinMaxdiff_Y = max.y() - min.y();
 
-		double diffCoord_X=(MinMaxdiff_X* p.getX())/width;
-		double diffCoord_Y=(MinMaxdiff_Y*p.getY())/height;
+        double diffCoord_X=(MinMaxdiff_X* p.getX())/width;
+        double diffCoord_Y=(MinMaxdiff_Y*p.getY())/height;
 
-		double FullCoord_X=diffCoord_X+this.min.x();
-		double FullCoord_Y=diffCoord_Y+this.min.y();
+        double FullCoord_X=diffCoord_X+this.min.x();
+        double FullCoord_Y=diffCoord_Y+this.min.y();
 
-		Point3D ThisPoint=new Point3D(FullCoord_X,FullCoord_Y);
-		return ThisPoint;
-	}
+        Point3D ThisPoint=new Point3D(FullCoord_X,FullCoord_Y);
+        return ThisPoint;
+    }
 
-	/**
-	 *function that convert coordinate(point of x and y) to pixel
-	 * @param cords point in degree(x,y)
-	 * @return point pixel
-	 */
+    /**
+     *function that convert coordinate(point of x and y) to pixel
+     * @param cords point in degree(x,y)
+     * @return point pixel
+     */
 
-	public Pixel CoordinateToPixel(Point3D cords)
-	{
-		double diffx = Math.abs(cords.x() - min.x());
-		double diffy = Math.abs(cords.y() - min.y());
+    public Pixel CoordinateToPixel(Point3D cords)
+    {
+        double diffx = Math.abs(cords.x() - min.x());
+        double diffy = Math.abs(cords.y() - min.y());
 
-		double MinMaxdiff_X = Math.abs(max.x() - min.x());
-		double MinMaxdiff_Y = Math.abs(max.y() - min.y());
 
-		double new_x = Math.abs((diffx * Pmax.getX()) / MinMaxdiff_X);
-		double new_y = Math.abs ((diffy * Pmax.getY()) / MinMaxdiff_Y);
+        double MinMaxdiff_X = Math.abs(max.x() - min.x());
+        double MinMaxdiff_Y = Math.abs(max.y() - min.y());
 
-		new_x = Math.round(new_x);
-		new_y = Math.round(new_y);
 
-		return new Pixel((int)new_x,(int)new_y);
-	}
+        double new_x = Math.abs((diffx * Pmax.getX()) / MinMaxdiff_X);
+        double new_y = Math.abs ((diffy * Pmax.getY()) / MinMaxdiff_Y);
 
-	/**
-	 *function that convert coordinate(point of x and y) to pixel
-	 * @param cords point in degree(x,y)
-	 * @param width of the map that we get from the width of the map
-	 * @param height of the map that we get from the height of the map
-	 * @return pixel of x and y
-	 */
-	public Pixel CoordinateToPixel(Point3D cords , int width, int height)
-	{
+
+        new_x = Math.round(new_x);
+        new_y = Math.round(new_y);
+
+        return new Pixel((int)new_x,(int)new_y);
+    }
+
+    /**
+     *function that convert coordinate(point of x and y) to pixel
+     * @param cords point in degree(x,y)
+     * @param width of the map that we get from the width of the map
+     * @param height of the map that we get from the height of the map
+     * @return pixel of x and y
+     */
+    public Pixel CoordinateToPixel(Point3D cords , int width, int height)
+    {
         double diffx = cords.x() - min.x();
         double diffy = cords.y() - min.y();
 
@@ -140,20 +143,21 @@ public class Map
         double new_x = Math.abs((diffx * width) / MinMaxdiff_X);
         double new_y = Math.abs((diffy * height) / MinMaxdiff_Y);
 
-		new_x = Math.round(new_x);
-		new_y = Math.round(new_y);
+        new_x = Math.round(new_x);
+        new_y = Math.round(new_y);
 
         return new Pixel((int)new_x,(int)new_y);
-	}
+    }
 
 
-	/**
-	 * haversine formula that
-	 * @param p1
-	 * @param p2
-	 * @return
-	 */
-	//https://rosettacode.org/wiki/Haversine_formula#Java.
+    /**
+     * haversine formula that
+     * @param p1
+     * @param p2
+     * @return
+     */
+    //https://rosettacode.org/wiki/Haversine_formula#Java.
+	/*
 	public double haversine(Pixel p1, Pixel p2) {
 
 		Point3D Pcoord1= new Point3D(this.PixelToCoordinate( p1));
@@ -170,32 +174,43 @@ public class Map
 		double c = 2 * Math.asin(Math.sqrt(a));
 		return R * c;
 	}
+	*/
+    /**
+     * this method return the angle between two points of pixel
+     * @param p1 in pixel
+     * @param p2 in pixel
+     * @return the angle in radian
+     */
 
-	//https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
+    //https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
 
-	public double Azimuth(Pixel p1, Pixel p2) {
+    public double Azimuth(Pixel p1, Pixel p2) {
 
-		Point3D Pcoord1= new Point3D(this.PixelToCoordinate( p1));
-		Point3D Pcoord2= new Point3D(this.PixelToCoordinate( p2));
+        Point3D Pcoord1= new Point3D(this.PixelToCoordinate( p1));
+        Point3D Pcoord2= new Point3D(this.PixelToCoordinate( p2));
 
-		double x=Math.cos(Pcoord2.x())*Math.sin(Pcoord2.y()-Pcoord1.y());
-		double y=(Math.cos(Pcoord1.x())*Math.sin(Pcoord2.x()))
-				-(Math.sin(Pcoord1.x())*Math.cos(Pcoord2.x()*Math.cos(Pcoord2.y()-Pcoord1.y())));
+        double x=Math.cos(Pcoord2.x())*Math.sin(Pcoord2.y()-Pcoord1.y());
+        double y=(Math.cos(Pcoord1.x())*Math.sin(Pcoord2.x()))
+                -(Math.sin(Pcoord1.x())*Math.cos(Pcoord2.x()*Math.cos(Pcoord2.y()-Pcoord1.y())));
 
-		double angle=Math.atan2(x, y);
-		return angle;
-	}
+        double angle=Math.abs(Math.atan2(x, y));
+        return angle;
+    }
 
-	public Pixel getPmax()
-	{
-		return this.Pmax;
-	}
+    public Pixel getPmax()
+    {
+        return this.Pmax;
+    }
 
-	public double SizeOfPixleInMeter()
-	{
-		MyCoords c = new MyCoords();
-		Point3D firstP = PixelToCoordinate(new Pixel(642 , 0));
-		Point3D SecondP = PixelToCoordinate(new Pixel(642 , 1));
-		return c.distance3d(firstP , SecondP);
-	}
+    /**
+     * caliculate how much pixel is in meter
+     * @return size of 1 pixel in meters
+     */
+    public double SizeOfPixleInMeter()
+    {
+        MyCoords c = new MyCoords();
+        Point3D firstP = PixelToCoordinate(new Pixel(642 , 0));
+        Point3D SecondP = PixelToCoordinate(new Pixel(642 , 1));
+        return c.distance3d(firstP , SecondP);
+    }
 }
